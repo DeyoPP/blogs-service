a5a263778f127bfa95d9e4120b0c1c8e49f297c3
dejan <dejan.pelemis@cyberlab.rs>
2024-08-14 12:58:10 +0200

    Update

--- model/Blog.go
+++ model/Blog.go
@@ -5,6 +5,7 @@ package model
 // 	//"github.com/lib/pq"
 // )
 
+// Uncomment the following if you intend to use them
 // type BlogStatus int
 // const (
 // 	Draft BlogStatus = iota
@@ -23,14 +24,14 @@ package model
 // 	Tips
 // 	Culture
 // 	Accomodation
-)
+// )
 
 type Blog struct {
 	ID          int       `gorm:"primaryKey;autoIncrement" json:"id"`          // Primary key with auto-increment
-	Name        string        `gorm:"column:name;type:varchar(255)" json:"name"`        // Column name and type
-	Description string        `gorm:"column:description;type:text" json:"description"` // Column name and type
-	// CreationTime 	time.Time 		`json:"creationTime"`
-	// Image			string			`json:"image"`
-	// Status 			BlogStatus		`json:"status"`
-	// Category		BlogCategory	`json:"category"`
+	Name        string    `gorm:"column:name;type:varchar(255)" json:"name"`   // Column name and type
+	Description string    `gorm:"column:description;type:text" json:"description"` // Column name and type
+	// CreationTime time.Time `json:"creationTime"`
+	// Image        string    `json:"image"`
+	// Status       BlogStatus `json:"status"`
+	// Category     BlogCategory `json:"category"`
 }
